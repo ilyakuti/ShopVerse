17e1a06b39e5c645421c378a44a3a0f85b9b4d6f
ilya <eliyakooti@gmail.com>
2026-06-28 23:17:15 +0330

    Enhance product model and detail view: add new fields for description, brand, battery, weight, model, connectivity, and warranty; update product detail template to display these attributes.

--- store/models.py
+++ store/models.py
@@ -8,6 +8,13 @@ class Product(models.Model):
     price = models.DecimalField (max_digits=10, decimal_places=2, null=True, verbose_name="Product Price")
     discount = models.DecimalField (max_digits=10, decimal_places=2, null=True, blank=True, verbose_name="Discount")
     product_category = models.ManyToManyField("ProductCategory")
+    description = models.TextField (null=True, verbose_name="Product Description")
+    brand = models.CharField (max_length=127, null=True, verbose_name="Product Brand")
+    battery = models.CharField (max_length=127, null=True, verbose_name="Product Battery", blank=True)
+    weight = models.CharField (max_length=127, null=True, verbose_name="Product Weight", blank=True)
+    product_model = models.CharField (max_length=127, null=True, verbose_name="Product Model", blank=True)
+    connectivity = models.CharField (max_length=127, null=True, verbose_name="Product Connectivity", blank=True)
+    warranty = models.CharField (max_length=127, null=True, verbose_name="Product Warranty", blank=True)
     def __str__(self):
         return self.name
 
