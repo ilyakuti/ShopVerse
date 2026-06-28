a318e1d7850e8487470e84e1048d4328335761bf
ilya <eliyakooti@gmail.com>
2026-06-28 22:06:56 +0330

    Add product detail view and enhance homepage with category filtering and search functionality

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import homepage
+from .views import homepage, detail_page
 
 urlpatterns = [
     path ('', homepage, name="homepage_url"),
+    path ('product/<int:product_id>/', detail_page, name="detail_page_url"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,19 +1,31 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, ProductCategory
 
 # Create your views here.
 
-def homepage (request):
-    product = Product.objects.all()
-    q = request.GET.get ('q')
+def homepage(request):
+    products = Product.objects.all()
+    q = request.GET.get('q')
 
     if q:
-        product = product.filter(name__icontains=q)
-    else:
-        product = Product.objects.all()
+        products = products.filter(name__icontains=q)
 
+    category_name = request.GET.get('category')
+    if category_name:
+        products = products.filter(product_category__name=category_name)
+
+    categories = ProductCategory.objects.all()
     context = {
-        "product":product
+        "products": products,
+        "categories": categories,
+        "selected_category": category_name,
     }
 
     return render (request, "index.html", context)
+
+def detail_page (request, product_id):
+    product = Product.objects.get(id=product_id)
+    context = {
+        "product": product
+    }
+    return render(request, "product-detail.html", context)
